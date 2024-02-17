fc8c09b3480a1bb036e92c1dfecc18c995c3d6c0
Ghali Boucetta <gboucett@student.42.fr>
2024-02-17 22:34:33 +0100

    Adding P3, for now everything should be working on first and third hosts, but the second one doesn't, why?

---
+++ P3/vtysh-conf/gboucett-4.sh
@@ -0,0 +1,27 @@
+#/bin/bash
+
+vtysh << EOF
+configure terminal
+
+hostname gboucett-4
+no ipv6 forwarding
+
+interface eth2
+    ip address 10.1.1.10/30
+    ip ospf area 0
+
+interface lo
+    ip address 1.1.1.4/32
+    ip ospf area 0
+
+router bgp 1
+    neighbor 1.1.1.1 remote-as 1
+    neighbor 1.1.1.1 update-source lo
+
+address-family l2vpn evpn
+    neighbor 1.1.1.1 activate
+    advertise-all-vni
+exit-address-family
+
+router ospf
+EOF
